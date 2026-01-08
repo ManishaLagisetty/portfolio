a5dafec8fa099928cf2b2a88ad92a3aad4e8810d
Sai Rahul <lagishettyrahul03@gmail.com>
2026-01-08 10:07:40 +0530

    updated arkos health info

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -18,7 +18,7 @@ export const Experience = () => {
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role} (${historyItem.organisation})`}</h3>
                   {/* <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p> */}
-                  <p>Analyzed intricate healthcare data to derive actionable insights and implemented SQL-based solutions, Python scripts, and ETL processes to streamline operations. Created KPIs and dashboards to drive business performance and collaborated with cross-functional teams to achieve enhanced results.</p>
+                  <p>{historyItem.description}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
                       return <li key={id}>{experience}</li>;
